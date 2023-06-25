31ccf232e7f0f611a6f7a6013a58fae7c3a6a3bc
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2023-06-24 20:53:13 -0700

    Added user can put marker on map
    Not using react maps gl

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,36 +4,41 @@ import { LoginView } from "../login-view/login-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 import { HomeView } from "../home-view/home-view";
 import { MapView } from "../map-view/map-view";
+import { useState } from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
+import ReactMapGL from "react-map-gl";
 
 export const MainView = () => {
+  const [lng, setLng] = useState(-122.4194); // SF longitude
+  const [lat, setLat] = useState(37.7749); // SF latitude
+  const [zoom, setZoom] = useState(12); // Zoom for SF
+
   return (
     <BrowserRouter>
       <NavigationBar />
-      <Container className="content-container">
-        <Routes>
-          <Route path="/" element={<MapView />} />
-
-          <Route
-            path="/login"
-            element={
-              <Row className="justify-content-md-center align-items-center mt-5">
-                <Col sm={5}>
-                  <Card className="p-3">
-                    <Card.Body>
-                      <LoginView />
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            }
-          />
-          <Route path="/map" element={<MapView />} />
-        </Routes>
-        <footer className="footer">
-          Icons by
-          <a href="https://icons8.com"> Icons8</a>.
-        </footer>
-      </Container>
+      {/* <Container className="content-container"> */}
+      <Routes>
+        <Route path="/" element={<MapView />} />
+        <Route
+          path="/login"
+          element={
+            <Row className="justify-content-md-center align-items-center mt-5">
+              <Col sm={5}>
+                <Card className="p-3">
+                  <Card.Body>
+                    <LoginView />
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          }
+        />
+      </Routes>
+      <footer className="footer">
+        Icons by
+        <a href="https://icons8.com"> Icons8</a>.
+      </footer>
+      {/* </Container> */}
     </BrowserRouter>
   );
 };
