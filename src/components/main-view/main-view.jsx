e50251f83595579120d157bed8531eaa77150686
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2023-06-27 18:30:38 -0500

    Completed static sidebar

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,11 +7,9 @@ import { HomeView } from "../home-view/home-view";
 import { MapView } from "../map-view/map-view";
 import { useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
-// import mapboxgl from "mapbox-gl";
-import ReactMapGL from "react-map-gl";
-// import { NavigationBar } from "../navigation-bar/navigation-bar";
+import "bootstrap/js/dist/dropdown";
 
-// remember to save as variable
+// remember to save Mapbox key as variable
 
 export const MainView = () => {
   const [viewport, setViewport] = useState({
@@ -28,28 +26,29 @@ export const MainView = () => {
     <BrowserRouter>
       <div className="container-fluid">
         <div className="row">
-          <Col sm={2}>
+          <Col style={{ padding: 0 }} sm={2}>
             <Sidebar />
           </Col>
 
           {/* <NavigationBar /> */}
-          <Col sm={10}>
+          <Col style={{ padding: 0 }} sm={10}>
             <Routes>
               <Route
                 path="/"
                 element={
-                  <div style={{ width: "100%", height: "100%" }}>
-                    <ReactMapGL
-                      mapboxApiAccessToken="pk.eyJ1IjoicmFlZGF3bmxhdyIsImEiOiJjbGozMnRlMmoxaWdwM2V1dTI1OXE3djN3In0.Yv7jHogURr0wDVNobpeRiw"
-                      width="100%"
-                      height="100%"
-                      {...viewport}
-                      onViewportChange={handleViewportChange}
-                      mapStyle={
-                        "mapbox://styles/raedawnlaw/clj7q5u3m00a001nw8arocm3y"
-                      }
-                    />
-                  </div>
+                  <MapView />
+                  // <div style={{ width: "100%", height: "100%" }}>
+                  //   <ReactMapGL
+                  //     mapboxApiAccessToken="pk.eyJ1IjoicmFlZGF3bmxhdyIsImEiOiJjbGozMnRlMmoxaWdwM2V1dTI1OXE3djN3In0.Yv7jHogURr0wDVNobpeRiw"
+                  //     width="100%"
+                  //     height="100%"
+                  //     {...viewport}
+                  //     onViewportChange={handleViewportChange}
+                  //     mapStyle={
+                  //       "mapbox://styles/raedawnlaw/clj7q5u3m00a001nw8arocm3y"
+                  //     }
+                  //   />
+                  // </div>
                 }
               />
               <Route
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,20 +1,64 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-auto col-sm-2 bg-light d-flex flex-column justify-content-between min-vh-100">
-          <div>
-            <a className="text-decoration-none ms-4 d-flex align-items-center d-none d-sm-inline">
-              <span className="fs-4">Side Menu</span>
+    <div className="bg-light d-flex flex-column align-items-center justify-content-between min-vh-100">
+      <div className="mt-4">
+        <a className=" text-decoration-none d-flex align-items-center d-none d-sm-inline">
+          <span className="fs-4 sidebar-brand">OtherForest</span>
+        </a>
+        <hr className="d-none d-sm-block"></hr>
+        <ul className="nav nav-pills flex-column">
+          <li className="nav-item d-flex align-items-center bullet-point">
+            <a href="#" className="nav-link" aria-current="page">
+              <span style={{ color: "rgb(70, 124, 67)" }}>Dashboard</span>
             </a>
-          </div>
-        </div>
+          </li>
+          <li className="nav-item bullet-point">
+            <a href="#" className="nav-link" aria-current="page">
+              <i
+                style={{ color: "rgb(70, 124, 67)" }}
+                className="bi bi-person me-2"
+              ></i>
+              <span style={{ color: "rgb(70, 124, 67)" }}>Profile</span>
+            </a>
+          </li>
+          <li className="nav-item bullet-point">
+            <a href="#" className="nav-link" aria-current="page">
+              <i
+                style={{ color: "rgb(70, 124, 67)" }}
+                className="bi bi-question-circle me-2"
+              ></i>
+              <span style={{ color: "rgb(70, 124, 67)" }}>Help</span>
+            </a>
+          </li>
+        </ul>
       </div>
+      {/* <div className="dropdown open">
+        <a
+          className="btn border-none dropdown-toggle"
+          type="button"
+          id="triggerId"
+          data-bs-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <i className="bi bi-person"></i>
+          <span>Username</span>
+        </a>
+
+        <div className="dropdown-menu" aria-labelledby="triggerId">
+          <a className="dropdown-item" href="#">
+            Profile
+          </a>
+          <a className="dropdown-item" href="#">
+            Settings
+          </a>
+        </div>
+      </div> */}
     </div>
   );
 };
