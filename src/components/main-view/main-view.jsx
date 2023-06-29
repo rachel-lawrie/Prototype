0199282847b710b7a4f1c033a496626366c4a14c
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2023-06-29 15:51:39 -0700

    Created burger bar (menu does not expand yet)

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -24,55 +24,66 @@ export const MainView = () => {
 
   return (
     <BrowserRouter>
-      <div className="container-fluid">
-        <div className="row">
-          <Col style={{ padding: 0 }} sm={2}>
+      <div
+        className="container-fluid"
+        style={{
+          width: "100vw",
+          height: "100vh",
+          position: "relative",
+          padding: 0,
+        }}
+      >
+        {/* <div className="row"> */}
+        {/* <Col style={{ padding: 0 }} sm={2}>
             <Sidebar />
-          </Col>
-
-          {/* <NavigationBar /> */}
-          <Col style={{ padding: 0 }} sm={10}>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <MapView />
-                  // <div style={{ width: "100%", height: "100%" }}>
-                  //   <ReactMapGL
-                  //     mapboxApiAccessToken="pk.eyJ1IjoicmFlZGF3bmxhdyIsImEiOiJjbGozMnRlMmoxaWdwM2V1dTI1OXE3djN3In0.Yv7jHogURr0wDVNobpeRiw"
-                  //     width="100%"
-                  //     height="100%"
-                  //     {...viewport}
-                  //     onViewportChange={handleViewportChange}
-                  //     mapStyle={
-                  //       "mapbox://styles/raedawnlaw/clj7q5u3m00a001nw8arocm3y"
-                  //     }
-                  //   />
-                  // </div>
-                }
-              />
-              <Route
-                path="/login"
-                element={
-                  <Row className="justify-content-md-center align-items-center mt-5">
-                    <Col sm={5}>
-                      <Card className="p-3">
-                        <Card.Body>
-                          <LoginView />
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  </Row>
-                }
-              />
-            </Routes>
-          </Col>
+          </Col> */}
+        <div style={{ position: "absolute", zIndex: 10 }}>
+          <Sidebar />
         </div>
 
-        <footer className="footer">
+        {/* <NavigationBar /> */}
+        {/* <Col style={{ padding: 0 }} sm={10}> */}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <MapView />
+              // <div style={{ width: "100%", height: "100%" }}>
+              //   <ReactMapGL
+              //     mapboxApiAccessToken="pk.eyJ1IjoicmFlZGF3bmxhdyIsImEiOiJjbGozMnRlMmoxaWdwM2V1dTI1OXE3djN3In0.Yv7jHogURr0wDVNobpeRiw"
+              //     width="100%"
+              //     height="100%"
+              //     {...viewport}
+              //     onViewportChange={handleViewportChange}
+              //     mapStyle={
+              //       "mapbox://styles/raedawnlaw/clj7q5u3m00a001nw8arocm3y"
+              //     }
+              //   />
+              // </div>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Row className="justify-content-md-center align-items-center mt-5">
+                <Col sm={5}>
+                  <Card className="p-3">
+                    <Card.Body>
+                      <LoginView />
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            }
+          />
+        </Routes>
+        {/* </Col> */}
+        {/* </div> */}
+
+        {/* <footer className="footer">
           Icons by
           <a href="https://icons8.com"> Icons8</a>.
-        </footer>
+        </footer> */}
       </div>
     </BrowserRouter>
   );
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,64 +1,79 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./sidebar.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Sidebar = () => {
-  return (
-    <div className="bg-light d-flex flex-column align-items-center justify-content-between min-vh-100">
-      <div className="mt-4">
-        <a className=" text-decoration-none d-flex align-items-center d-none d-sm-inline">
-          <span className="fs-4 sidebar-brand">OtherForest</span>
-        </a>
-        <hr className="d-none d-sm-block"></hr>
-        <ul className="nav nav-pills flex-column">
-          <li className="nav-item d-flex align-items-center bullet-point">
-            <a href="#" className="nav-link" aria-current="page">
-              <span style={{ color: "rgb(70, 124, 67)" }}>Dashboard</span>
-            </a>
-          </li>
-          <li className="nav-item bullet-point">
-            <a href="#" className="nav-link" aria-current="page">
-              <i
-                style={{ color: "rgb(70, 124, 67)" }}
-                className="bi bi-person me-2"
-              ></i>
-              <span style={{ color: "rgb(70, 124, 67)" }}>Profile</span>
-            </a>
-          </li>
-          <li className="nav-item bullet-point">
-            <a href="#" className="nav-link" aria-current="page">
-              <i
-                style={{ color: "rgb(70, 124, 67)" }}
-                className="bi bi-question-circle me-2"
-              ></i>
-              <span style={{ color: "rgb(70, 124, 67)" }}>Help</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-      {/* <div className="dropdown open">
-        <a
-          className="btn border-none dropdown-toggle"
-          type="button"
-          id="triggerId"
-          data-bs-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          <i className="bi bi-person"></i>
-          <span>Username</span>
-        </a>
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [menu_class, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
 
-        <div className="dropdown-menu" aria-labelledby="triggerId">
-          <a className="dropdown-item" href="#">
-            Profile
-          </a>
-          <a className="dropdown-item" href="#">
-            Settings
-          </a>
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("burger-bar unclicked");
+      setMenuClass("menu hidden");
+    }
+  };
+
+  return (
+    <div>
+      <nav>
+        <div className="burger-menu">
+          <div
+            className={`burger-bar ${burger_class}`}
+            onClick={updateMenu}
+          ></div>
+          <div
+            className={`burger-bar ${burger_class}`}
+            onClick={updateMenu}
+          ></div>
+          <div
+            className={`burger-bar ${burger_class}`}
+            onClick={updateMenu}
+          ></div>
         </div>
-      </div> */}
+      </nav>
+
+      <div className={menu_class}></div>
     </div>
+
+    // <div className="bg-light d-flex flex-column align-items-center justify-content-between min-vh-100">
+    //   <div className="mt-4">
+    //     <a className=" text-decoration-none d-flex align-items-center d-none d-sm-inline">
+    //       <span className="fs-4 sidebar-brand">OtherForest</span>
+    //     </a>
+    //     <hr className="d-none d-sm-block"></hr>
+    //     <ul className="nav nav-pills flex-column">
+    //       <li className="nav-item d-flex align-items-center bullet-point">
+    //         <a href="#" className="nav-link" aria-current="page">
+    //           <span style={{ color: "rgb(70, 124, 67)" }}>Dashboard</span>
+    //         </a>
+    //       </li>
+    //       <li className="nav-item bullet-point">
+    //         <a href="#" className="nav-link" aria-current="page">
+    //           <i
+    //             style={{ color: "rgb(70, 124, 67)" }}
+    //             className="bi bi-person me-2"
+    //           ></i>
+    //           <span style={{ color: "rgb(70, 124, 67)" }}>Profile</span>
+    //         </a>
+    //       </li>
+    //       <li className="nav-item bullet-point">
+    //         <a href="#" className="nav-link" aria-current="page">
+    //           <i
+    //             style={{ color: "rgb(70, 124, 67)" }}
+    //             className="bi bi-question-circle me-2"
+    //           ></i>
+    //           <span style={{ color: "rgb(70, 124, 67)" }}>Help</span>
+    //         </a>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </div>
   );
 };
