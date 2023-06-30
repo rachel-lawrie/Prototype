bd7a9b6e6af66a2c0c1c95a41d4b4a930e295da8
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2023-06-29 20:36:19 -0700

    Added button to make menu visible when clicked

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -14,66 +14,62 @@ export const Sidebar = () => {
     if (!isMenuClicked) {
       setBurgerClass("burger-bar clicked");
       setMenuClass("menu visible");
+      setIsMenuClicked(true);
     } else {
       setBurgerClass("burger-bar unclicked");
       setMenuClass("menu hidden");
+      setIsMenuClicked(false);
     }
   };
 
   return (
     <div>
       <nav>
-        <div className="burger-menu">
-          <div
-            className={`burger-bar ${burger_class}`}
-            onClick={updateMenu}
-          ></div>
-          <div
-            className={`burger-bar ${burger_class}`}
-            onClick={updateMenu}
-          ></div>
-          <div
-            className={`burger-bar ${burger_class}`}
-            onClick={updateMenu}
-          ></div>
+        <div className="burger-menu" onClick={updateMenu}>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
         </div>
       </nav>
 
-      <div className={menu_class}></div>
+      <div
+        className={`${menu_class} bg-light  flex-column align-items-center justify-content-between min-vh-100`}
+      >
+        <button className="close-btn" onClick={updateMenu}>
+          X
+        </button>
+        <div className="m-5">
+          <a className=" text-decoration-none d-flex align-items-center d-none d-sm-inline">
+            <span className="fs-4 sidebar-brand">OtherForest</span>
+          </a>
+          <hr className="d-none d-sm-block"></hr>
+          <ul className="nav nav-pills flex-column">
+            <li className="nav-item d-flex align-items-center bullet-point">
+              <a href="#" className="nav-link" aria-current="page">
+                <span style={{ color: "rgb(70, 124, 67)" }}>Dashboard</span>
+              </a>
+            </li>
+            <li className="nav-item bullet-point">
+              <a href="#" className="nav-link" aria-current="page">
+                <i
+                  style={{ color: "rgb(70, 124, 67)" }}
+                  className="bi bi-person me-2"
+                ></i>
+                <span style={{ color: "rgb(70, 124, 67)" }}>Profile</span>
+              </a>
+            </li>
+            <li className="nav-item bullet-point">
+              <a href="#" className="nav-link" aria-current="page">
+                <i
+                  style={{ color: "rgb(70, 124, 67)" }}
+                  className="bi bi-question-circle me-2"
+                ></i>
+                <span style={{ color: "rgb(70, 124, 67)" }}>Help</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-
-    // <div className="bg-light d-flex flex-column align-items-center justify-content-between min-vh-100">
-    //   <div className="mt-4">
-    //     <a className=" text-decoration-none d-flex align-items-center d-none d-sm-inline">
-    //       <span className="fs-4 sidebar-brand">OtherForest</span>
-    //     </a>
-    //     <hr className="d-none d-sm-block"></hr>
-    //     <ul className="nav nav-pills flex-column">
-    //       <li className="nav-item d-flex align-items-center bullet-point">
-    //         <a href="#" className="nav-link" aria-current="page">
-    //           <span style={{ color: "rgb(70, 124, 67)" }}>Dashboard</span>
-    //         </a>
-    //       </li>
-    //       <li className="nav-item bullet-point">
-    //         <a href="#" className="nav-link" aria-current="page">
-    //           <i
-    //             style={{ color: "rgb(70, 124, 67)" }}
-    //             className="bi bi-person me-2"
-    //           ></i>
-    //           <span style={{ color: "rgb(70, 124, 67)" }}>Profile</span>
-    //         </a>
-    //       </li>
-    //       <li className="nav-item bullet-point">
-    //         <a href="#" className="nav-link" aria-current="page">
-    //           <i
-    //             style={{ color: "rgb(70, 124, 67)" }}
-    //             className="bi bi-question-circle me-2"
-    //           ></i>
-    //           <span style={{ color: "rgb(70, 124, 67)" }}>Help</span>
-    //         </a>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </div>
   );
 };
